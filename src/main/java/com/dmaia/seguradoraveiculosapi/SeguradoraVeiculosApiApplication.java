348b82e7a50b56eb26755293a879a46ea9244049
Dowglas Maia <dowglasmaia@gmail.com>
2021-01-26 16:25:26 -0300

    Criando Projeto

---
+++ src/main/java/com/dmaia/seguradoraveiculosapi/SeguradoraVeiculosApiApplication.java
@@ -0,0 +1,13 @@
+package com.dmaia.seguradoraveiculosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeguradoraVeiculosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeguradoraVeiculosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dmaia/seguradoraveiculosapi/SeguradoraVeiculosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmaia.seguradoraveiculosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeguradoraVeiculosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
